754a91358eeb113bc0d0720e7b467beb3ba5566b
Celso Zabala <chelso@MacBook-Air-de-Celso.local>
2023-03-10 10:54:27 -0300

    Initial commit

---
+++ server/src/main/java/com/chelso/expensemate/ExpenseMateApplication.java
@@ -0,0 +1,13 @@
+package com.chelso.expensemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMateApplication.class, args);
+	}
+
+}
